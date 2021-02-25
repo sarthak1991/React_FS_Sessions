d487359976300b2eda2782dc2073c47e16108a6b
Sarthak Batra <Sarthakbatra@rocketmail.com>
2021-02-25 21:59:58 +0530

    useState and use Effect with functional components

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,54 +1,96 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export default class Contact extends Component {
+// export default class Contact extends Component {
 
-    state = {
-        name: "Honnaraju", //Changed this state on event
-        posts: [] // Changed this state on lifecycle method
+//     state = {
+//         name: "Honnaraju", //Changed this state on event
+//         posts: [] // Changed this state on lifecycle method
+//     }
+
+//     changeName = () => {
+//         console.clear()
+//         this.setState({
+//             name: "Sarthak"
+//         })
+//     }
+
+//     componentDidMount = async () => {
+//         try {
+//             let res = await fetch("http://jsonplaceholder.typicode.com/posts");
+//             let articles = await res.json();
+//             this.setState(prevState => {
+//                 return ({
+//                     posts: articles
+//                 })
+//             })
+//         } catch (err) {
+//             console.log(err)
+//         }
+
+//     }
+
+//     render() {
+//         console.log("This is the state");
+//         { this.state.posts.map(post => console.log(post.id)) }
+//         return (
+//             <div>
+//                 <p>Hello, my name is {this.state.name}</p>
+//                 <button onClick={this.changeName}>change the name</button>
+
+//                 <hr></hr>
+
+//                 <div>
+//                     {this.state.posts.map(singlePost => {
+//                         return (<div key={singlePost.id}>
+//                             <hr></hr>
+//                             <div>This is post number {singlePost.id}</div>
+//                             <div>This is the title: {singlePost.title}</div>
+//                             <hr></hr>
+//                         </div>)
+//                     }
+//                     )
+//                     }
+
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
+
+import { useState, useEffect } from "react"
+
+const Contact = () => {
+    const [nameOfBatchmate, setNameOfBatchmate] = useState("Honnaraju")
+    const [posts, setPosts] = useState([])
+
+    useEffect(async () => {
+        try {
+            let res = await fetch("http://jsonplaceholder.typicode.com/posts");
+            let articles = await res.json()
+            setPosts(articles)
+        } catch (error) {
+            console.log(error)
+        }
+    }, [])
+
+    const clickHandler = () => {
+        setNameOfBatchmate("Sarthak")
     }
-
-    changeName = () => {
-        console.clear()
-        this.setState({
-            name: "Sarthak"
-        })
-    }
-
-    componentDidMount = () => {
-        fetch("http://jsonplaceholder.typicode.com/posts")
-            .then(res => res.json())
-            .then(articles => this.setState(prevState => {
-                return ({
-                    posts: articles
-                })
-            }))
-            .catch(err => console.log(err))
-    }
-
-    render() {
-        console.log("This is the state");
-        { this.state.posts.map(post => console.log(post.id)) }
-        return (
-            <div>
-                <p>Hello, my name is {this.state.name}</p>
-                <button onClick={this.changeName}>change the name</button>
-
-                <hr></hr>
-
-                <div>
-                    {this.state.posts.map(singlePost => {
-                        return (<div>
-                            <hr></hr>
-                            <div>This is post number {singlePost.id}</div>
-                            <div>This is the title: {singlePost.title}</div>
-                            <hr></hr>
-                        </div>)
-                    }
-                    )
-                    }
-
-                </div>
-            </div>
-        )
-    }
+    return (
+        <div>
+            Hello, my functional component name is {nameOfBatchmate}
+            <button onClick={clickHandler}>Click me</button>
+            {posts.map(singlePost => {
+                return (
+                    <div>
+                        <hr></hr>
+                        <p>this is the post id = {singlePost.id}</p>
+                        <p>title = {singlePost.title}</p>
+                        <hr></hr>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
+export default Contact;
